0c5aa24d30f1b83d456a6eb19a79720909dbc500
Brad Rice <bradley_rice@mac.com>
2014-08-24 17:20:42 -0500

    Change name of Heroku app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'punchy.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'punchy-timecard.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     enable_starttls_auto: true,
